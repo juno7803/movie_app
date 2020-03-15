534e3c61388ec0ae24150e0601a723ff0b793778
junho <juno7803@naver.com>
2020-03-15 22:27:11 +0900

    #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React from 'react';
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("hello constructor!");
-  }
-  state={
-    count: 0
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
   };
-  add = () =>{
-    this.setState(current => ({count: current.count +=1}));
-  };// 이게 맞는 표현
-  minus =() =>{
-    this.setState({count: this.state.count -=1});
-  };// 문법적으론 맞지만 state의 목적에 부합하지 않는 표현
-  // kind of member func
-  componentDidMount(){
-    console.log("component rendered");
-  } // life cycle method(mounting)
-  componentDidUpdate(){
-    console.log("I just updated");
-  } // life cycle method(updating)
-  componentWillUnmount(){
-    console.log("component Bye bye");
-  } // life cycle method(unmounting) 
-  render(){
-    console.log("I am rendering");
-    return(
+  componentDidMount() {
+    setTimeout(()=>{
+      this.setState({isLoading: false});
+    },6000);// javascript func : setTimeout(callback,시간)
+  }
+  render() {
+    const {isLoading} = this.state; // es6 문법->아래에서 this.state.isLoading 할필요 없어짐
+    return (
       <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-        {/* onClick={} is React magic */}
+        {isLoading ? "Loading..." : "We are ready"}
       </div>
     )
   }
