229196da5b9c24af7648e27d1c9a0c412264243f
junho <juno7803@naver.com>
2020-03-15 20:57:17 +0900

    #3.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import ProtoTypes from 'prop-types'; // to check props send correctly
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hello constructor!");
+  }
   state={
     count: 0
   };
@@ -11,9 +14,18 @@ class App extends React.Component{
   minus =() =>{
     this.setState({count: this.state.count -=1});
   };// 문법적으론 맞지만 state의 목적에 부합하지 않는 표현
-  
   // kind of member func
+  componentDidMount(){
+    console.log("component rendered");
+  } // life cycle method(mounting)
+  componentDidUpdate(){
+    console.log("I just updated");
+  } // life cycle method(updating)
+  componentWillUnmount(){
+    console.log("component Bye bye");
+  } // life cycle method(unmounting) 
   render(){
+    console.log("I am rendering");
     return(
       <div>
         <h1>The number is {this.state.count}</h1>
