c7e8cbb649a0c3b812e795f50f44749db836027b
junho <juno7803@naver.com>
2020-03-17 01:03:49 +0900

    #4.1 Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import Axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
-  // async function getMovies() 라고 써야하지만, 멤버함수는 이렇게 쓸 수 없음
+  
   getMovies = async () =>{
-    const movies = await Axios.get("https://yts.mx/api/v2/list_movies.json");
-    // fetch api = axios 같은 역할
-  }// const getMovie = function smth() {} 과 같은 느낌
+    const {data:{
+      data:{movies}
+      }}= await Axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating"); 
+    this.setState({movies: movies,isLoading: false}); // setState안의 state와 원소가 이름이 같다면 setState({movies}); 라고 써줘도 동일하다!(es6 문법)
+  };
   
   componentDidMount() {
-    this.getMovies();
-    console.log(this);  
+    this.getMovies(); // 여기서의 this = App
   }
 
   render() {
-    const {isLoading} = this.state; // es6 문법->아래에서 this.state.isLoading 할필요 없어짐
+    const {isLoading,movies} = this.state; // es6 문법->아래에서 this.state.isLoading 할필요 없어짐
     return (
       <div>
-        {isLoading ? "Loading..." : "We are ready"}
+        {isLoading ? "Loading..." : movies.map(param=>(
+          <Movie
+            key={param.id}
+            id={param.id}
+            year={param.year}
+            title={param.title}
+            summary={param.summary}
+            poster={param.medium_cover_image}
+          />
+        ))}
       </div>
-    )
+    );
   }
 }
 
