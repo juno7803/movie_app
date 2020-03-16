8a67a658cd016a2efbf04b871178a4c94f706b53
junho <juno7803@naver.com>
2020-03-17 00:13:19 +0900

    4.0 Fetching Movies from API(using Axios,aynsc~await)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import Axios from 'axios';
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
+  // async function getMovies() 라고 써야하지만, 멤버함수는 이렇게 쓸 수 없음
+  getMovies = async () =>{
+    const movies = await Axios.get("https://yts.mx/api/v2/list_movies.json");
+    // fetch api = axios 같은 역할
+  }// const getMovie = function smth() {} 과 같은 느낌
+  
   componentDidMount() {
-    setTimeout(()=>{
-      this.setState({isLoading: false});
-    },6000);// javascript func : setTimeout(callback,시간)
+    this.getMovies();
+    console.log(this);  
   }
+
   render() {
     const {isLoading} = this.state; // es6 문법->아래에서 this.state.isLoading 할필요 없어짐
     return (
